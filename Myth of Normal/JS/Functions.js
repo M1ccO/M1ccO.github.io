f587423f9551dceaf513b2b7f274214edc216b0f
M1ccO <140800391+M1ccO@users.noreply.github.com>
2023-12-20 10:51:04 +0200

    Update Functions.js

--- Myth of Normal/JS/Functions.js
+++ Myth of Normal/JS/Functions.js
@@ -1,11 +1,6 @@
 // Functions.js
 
 
-function navigateBack() {
-    swup.loadPage({
-        url: '../Index.html', // Change this URL to the correct path of your Index.html page
-        method: 'GET'
-    });
-}
+
 
 
