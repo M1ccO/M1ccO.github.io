b4a9e24d97419270b1e76f61ef0f6253174ee1e9
M1ccO <140800391+M1ccO@users.noreply.github.com>
2023-12-21 13:16:20 +0200

    Update Functions.js

--- Myth of Normal/JS/Functions.js
+++ Myth of Normal/JS/Functions.js
@@ -1,15 +1,21 @@
 // Functions.js
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    // You can safely call scrollToElement or any DOM manipulation functions here
+    scrollToElement('someElementId');
+
+    // More DOM manipulations or event listener attachments can go here
+});
 
 function scrollToElement(elementId) {
     var element = document.getElementById(elementId);
     if (element) {
         var topPosition = element.getBoundingClientRect().top + window.pageYOffset;
-
-        // Scroll to the element
         window.scrollTo({ top: topPosition, behavior: 'auto' });
     }
 }
+
+
 
 
 
