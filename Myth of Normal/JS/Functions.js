71fcb9b88adef784eaed2783d77e649f35ecf71d
M1ccO <140800391+M1ccO@users.noreply.github.com>
2023-12-23 12:22:39 +0200

    Update Functions.js

--- Myth of Normal/JS/Functions.js
+++ Myth of Normal/JS/Functions.js
@@ -1,6 +1,15 @@
 // Functions.js
 
 
+const swup = new Swup({
+  elements: [
+    // Specify the selectors Swup should handle
+    'a[href^="/"]:not([href^="#"])', // All internal links except anchor links
+    // Add other selectors as needed
+  ]
+});
+
+
 
 
 
