6480b21116bdaa6d0e48f48db5f89d0780137fd7
M1ccO <140800391+M1ccO@users.noreply.github.com>
2023-12-23 12:34:51 +0200

    Update swup.js

--- Myth of Normal/JS/swup.js
+++ Myth of Normal/JS/swup.js
@@ -2,3 +2,17 @@
 
 const swup = new Swup();
 
+import Swup from 'swup';
+import SwupScrollPlugin from 'swup/plugins/scrollPlugin';
+
+const swup = new Swup({
+  plugins: [new SwupScrollPlugin({
+    doScrollingRightAway: false,
+    animateScroll: true,
+    scrollFriction: 0.3,
+    scrollAcceleration: 0.04
+  })],
+  linkSelector: 'a[href^="/"]:not([data-no-swup]), a[href^="#"]'
+});
+
+
