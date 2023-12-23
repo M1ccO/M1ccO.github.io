54782b71bfb25b163198796ef9730a81e6f17f55
M1ccO <140800391+M1ccO@users.noreply.github.com>
2023-12-23 10:30:21 +0200

    Update Functions.js

--- Myth of Normal/JS/Functions.js
+++ Myth of Normal/JS/Functions.js
@@ -1,10 +1,6 @@
 // Functions.js
 
-var scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 800,
-    offset: 80, // or the height of your fixed header if any
-    // other options...
-});
+
 
 
 
