2726341b6edd6a25503e532e8c3130211ce5e1db
M1ccO <140800391+M1ccO@users.noreply.github.com>
2023-12-24 09:07:23 +0200

    Update Functions.js

--- Myth of Normal/JS/Functions.js
+++ Myth of Normal/JS/Functions.js
@@ -1,17 +1,7 @@
 // Functions.js
 
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        let target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
-    });
-});
+
 
 
 
