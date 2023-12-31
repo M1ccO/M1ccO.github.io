9570092d491d58ac4b44dde4495799eacaff51b5
M1ccO <140800391+M1ccO@users.noreply.github.com>
2023-12-31 13:28:37 +0200

    Update Search-Index.js

--- Myth of Normal/JS/Search-Index.js
+++ Myth of Normal/JS/Search-Index.js
@@ -37,3 +37,36 @@ function performSearch(query) {
 function redirectToSearchResults(query, type) {
     window.location.href = 'Content/search-results.html?query=' + encodeURIComponent(query) + '&type=' + type;
 }
+
+// Function to filter chapters based on search input
+function filterChapters(input) {
+    const chapters = document.querySelectorAll('.chapter-link'); // Adjust the selector as needed
+    const searchTerms = input.split(',').map(term => term.trim().toLowerCase());
+    let promises = [];
+
+    chapters.forEach(chapter => {
+        let chapterFile = chapter.getAttribute('href'); // Assuming this gets the correct file path
+        let promise = fetch(chapterFile)
+            .then(response => response.text())
+            .then(content => {
+                return { chapter: chapter, isMatch: searchTerms.some(term => content.toLowerCase().includes(term)) };
+            })
+            .catch(error => console.error('Error fetching chapter:', error));
+
+        promises.push(promise);
+    });
+
+    Promise.all(promises).then(results => {
+        results.forEach(result => {
+            result.chapter.style.display = result.isMatch ? "block" : "none";
+        });
+    });
+}
+
+// Modify the event listener for the search input
+document.getElementById("search-input").addEventListener("keyup", function(event) {
+    if (event.key === ',') {
+        const searchQuery = this.value;
+        filterChapters(searchQuery);
+    }
+});
