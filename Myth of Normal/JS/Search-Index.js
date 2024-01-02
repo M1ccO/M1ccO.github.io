4aecde44a8ae99c441a901794eca7abce646f550
M1ccO <140800391+M1ccO@users.noreply.github.com>
2024-01-02 07:31:00 +0200

    Update Search-Index.js

--- Myth of Normal/JS/Search-Index.js
+++ Myth of Normal/JS/Search-Index.js
@@ -4,17 +4,16 @@ const searchBar = document.querySelector('#searchContainer input');
 const searchContainer = document.querySelector('#searchContainer');
 const searchButton = document.querySelector('#searchContainer .search-btn');
 
-// Open search bar on focus
 searchBar.addEventListener('focus', function() {
     searchContainer.classList.remove('closed');
-    searchButton.disabled = false;
+    searchButton.removeAttribute('disabled');  // Enable the button
 });
 
-// Close search bar when it loses focus and no search query is entered
 searchBar.addEventListener('blur', function() {
+    // Only disable the button and close the search bar if there's no value in the search bar
     if (!searchBar.value.trim()) {
         searchContainer.classList.add('closed');
-        searchButton.disabled = true;
+        searchButton.setAttribute('disabled', '');  // Disable the button
     }
 });
 
