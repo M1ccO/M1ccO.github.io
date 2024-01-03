23215c47aca73250155a30b11e09f744fcfab7fc
M1ccO <140800391+M1ccO@users.noreply.github.com>
2024-01-03 12:55:35 +0200

    Update swup.js

--- Myth of Normal/JS/swup.js
+++ Myth of Normal/JS/swup.js
@@ -1,15 +1,1 @@
 // swup.js
-
-const swup = new Swup({
-    linkSelector: 'a:not([href^="#"])'
-});
-
-swup.on('contentReplaced', () => {
-    initializeSearchIndex();  // Reinitialize search functionalities
-    initializeButton();       // Reinitialize button functionalities
-});
-
-// Also call them on initial page load
-initializeSearchIndex();
-initializeButton();
-
