ea5813f66d21d381d5c47c5861b0c61af7987cc0
M1ccO <140800391+M1ccO@users.noreply.github.com>
2024-01-03 11:29:26 +0200

    Update Functions.js

--- Myth of Normal/JS/Functions.js
+++ Myth of Normal/JS/Functions.js
@@ -1,5 +1,25 @@
 // Functions.js
 
+var backToTopBtn = document.getElementById("button");
+
+setInterval(function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.opacity = 1;
+        backToTopBtn.style.transform = 'translateY(0)'; // Slide into view
+    } else {
+        backToTopBtn.style.opacity = 0;
+        backToTopBtn.style.transform = 'translateY(100%)'; // Slide out of view
+    }
+}, 100);
+
+backToTopBtn.onclick = function() {
+    var headingElement = document.querySelector(".heading");
+    if (headingElement) {
+        headingElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+};
 
 
 
