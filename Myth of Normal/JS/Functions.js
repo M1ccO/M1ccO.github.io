1dc7412cdf6ee387c7de020e21ac1338cfba23fb
M1ccO <140800391+M1ccO@users.noreply.github.com>
2024-01-04 09:32:39 +0200

    Update Functions.js

--- Myth of Normal/JS/Functions.js
+++ Myth of Normal/JS/Functions.js
@@ -1,6 +1,17 @@
 // Functions.js
 
+// Importing functions from other modules
 import { scrollToTop } from './Scroll-To-Top.js';
-import { initializeSearch } from './Search-Index.js';
+import { initializeSearchIndex } from './Search-Index.js';
 import { displaySearchResults } from './Search-Results.js';
+
+// Initialize 'Back to Top' button functionality
+scrollToTop();
+
+// Set up the search functionality on the index page
+initializeSearchIndex();
+
+// Process and display search results
+displaySearchResults();
+
 
