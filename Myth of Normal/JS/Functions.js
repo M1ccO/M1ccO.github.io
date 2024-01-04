fdbaf55affa672fd33fd1b86422b2197be47e97b
M1ccO <140800391+M1ccO@users.noreply.github.com>
2024-01-04 06:18:39 +0200

    Update Functions.js

--- Myth of Normal/JS/Functions.js
+++ Myth of Normal/JS/Functions.js
@@ -1,34 +1,24 @@
 // Functions.js
 
-
-function initializeButton() {
+document.addEventListener('DOMContentLoaded', (event) => {
     var backToTopBtn = document.getElementById("button");
 
-    // Only proceed if the button element exists
-    if (backToTopBtn) {
-        // Function to update button appearance based on scroll position
-        function updateButtonVisibility() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                backToTopBtn.style.opacity = 1;
-                backToTopBtn.style.transform = 'translateY(0)'; // Slide into view
-            } else {
-                backToTopBtn.style.opacity = 0;
-                backToTopBtn.style.transform = 'translateY(100%)'; // Slide out of view
-            }
+    setInterval(function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backToTopBtn.style.opacity = 1;
+            backToTopBtn.style.transform = 'translateY(0)'; // Slide into view
+        } else {
+            backToTopBtn.style.opacity = 0;
+            backToTopBtn.style.transform = 'translateY(100%)'; // Slide out of view
         }
+    }, 100);
 
-        // Set up the scroll event listener
-        window.addEventListener('scroll', updateButtonVisibility);
-
-        // Update button visibility initially
-        updateButtonVisibility();
-
-        // Set up the click event for the button
-        backToTopBtn.onclick = function() {
-            var headingElement = document.querySelector(".heading"); // Adjust the selector as needed
-            if (headingElement) {
-                headingElement.scrollIntoView();
-            }
-        };
-    }
-}
+    backToTopBtn.onclick = function() {
+        var headingElement = document.querySelector(".heading");
+        if (headingElement) {
+            headingElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    };
+});
