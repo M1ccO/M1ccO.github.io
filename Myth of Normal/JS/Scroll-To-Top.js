fb502d7ef37f4f8a52cf57a2a6fea6c1193ab729
M1ccO <140800391+M1ccO@users.noreply.github.com>
2024-01-04 11:06:38 +0200

    Update Scroll-To-Top.js

--- Myth of Normal/JS/Scroll-To-Top.js
+++ Myth of Normal/JS/Scroll-To-Top.js
@@ -1,31 +1,26 @@
 // Scroll-To-Top.js
 
-
-// backToTop.js
 function scrollToTop() {
-document.addEventListener('DOMContentLoaded', (event) => {
-    var backToTopBtn = document.getElementById("button");
+    document.addEventListener('DOMContentLoaded', (event) => {
+        var backToTopBtn = document.getElementById("button");
 
-    setInterval(function() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            backToTopBtn.style.opacity = 1;
-            backToTopBtn.style.transform = 'translateY(0)'; // Slide into view
-        } else {
-            backToTopBtn.style.opacity = 0;
-            backToTopBtn.style.transform = 'translateY(100%)'; // Slide out of view
-        }
-    }, 100);
+        setInterval(function() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                backToTopBtn.style.opacity = 1;
+                backToTopBtn.style.transform = 'translateY(0)'; // Slide into view
+            } else {
+                backToTopBtn.style.opacity = 0;
+                backToTopBtn.style.transform = 'translateY(100%)'; // Slide out of view
+            }
+        }, 100);
 
-    backToTopBtn.onclick = function() {
-        var headingElement = document.querySelector(".heading");
-        if (headingElement) {
-            headingElement.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
-    };
-}); 
+        backToTopBtn.onclick = function() {
+            // Scroll the page to the top immediately
+            window.scrollTo(0, 0);
+        };
+    });
 }
 
+// Export the function
 export { scrollToTop };
 
