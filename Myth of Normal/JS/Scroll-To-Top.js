bbf4151099a734dc5113d3e39d3060418a1085ef
M1ccO <140800391+M1ccO@users.noreply.github.com>
2024-01-05 13:07:24 +0200

    Update Scroll-To-Top.js

--- Myth of Normal/JS/Scroll-To-Top.js
+++ Myth of Normal/JS/Scroll-To-Top.js
@@ -1,9 +1,22 @@
 // Scroll-To-Top.js
 
 function scrollToTop() {
+    // Function to scroll to the top of the page
+    function smoothScrollToTop() {
+        if (document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0) {
+            window.scrollBy(0, -50);
+            setTimeout(smoothScrollToTop, 10);
+        }
+    }
+
+    // Add event listener once the DOM is fully loaded
     document.addEventListener('DOMContentLoaded', (event) => {
         var backToTopBtn = document.getElementById("button");
 
+        // Set the click event for the button
+        backToTopBtn.onclick = smoothScrollToTop;
+
+        // Check the scroll position at regular intervals
         setInterval(function() {
             if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
                 backToTopBtn.classList.add('show');
@@ -11,13 +24,10 @@ function scrollToTop() {
                 backToTopBtn.classList.remove('show');
             }
         }, 100);
-
-        backToTopBtn.onclick = function() {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        };
     });
 }
 
-// Export the function
+// Export the scrollToTop function
 export { scrollToTop };
+
 
