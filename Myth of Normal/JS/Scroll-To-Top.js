8bded9b4bbae50d309c18fa9a68f0239b1f5a30d
M1ccO <140800391+M1ccO@users.noreply.github.com>
2024-01-06 13:34:39 +0200

    Update Scroll-To-Top.js

--- Myth of Normal/JS/Scroll-To-Top.js
+++ Myth of Normal/JS/Scroll-To-Top.js
@@ -1,19 +1,23 @@
 // Scroll-Top-Top.js
 
 
-
 function scrollToChapter() {
     document.addEventListener('DOMContentLoaded', (event) => {
         var backToTopBtn = document.getElementById("button");
         var firstH3 = document.querySelector('h3');
+        var buttonClicked = false; // Flag to track button click
 
         function smoothScrollToChapter() {
             if (firstH3) {
                 firstH3.scrollIntoView({ behavior: 'smooth', block: 'start' });
-                // Hide the button after scrolling
-                backToTopBtn.style.opacity = 0;
-                backToTopBtn.style.transform = 'translateY(100%)';
+                hideButton();
             }
+        }
+
+        function hideButton() {
+            backToTopBtn.style.opacity = 0;
+            backToTopBtn.style.transform = 'translateY(100%)';
+            buttonClicked = true; // Set flag to true after hiding the button
         }
 
         backToTopBtn.onclick = smoothScrollToChapter;
@@ -21,9 +25,12 @@ function scrollToChapter() {
 
         setInterval(function() {
             if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                backToTopBtn.style.opacity = 1;
-                backToTopBtn.style.transform = 'translateY(0)'; // Slide into view
+                if (!buttonClicked) {
+                    backToTopBtn.style.opacity = 1;
+                    backToTopBtn.style.transform = 'translateY(0)'; // Slide into view
+                }
             } else {
+                buttonClicked = false; // Reset flag when at top of page
                 backToTopBtn.style.opacity = 0;
                 backToTopBtn.style.transform = 'translateY(100%)'; // Slide out of view
             }
@@ -32,3 +39,5 @@ function scrollToChapter() {
 }
 
 export { scrollToChapter };
+
+
