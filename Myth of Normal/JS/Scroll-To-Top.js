bf15a971a329fd5dab2eb60a5d4750a31679813d
M1ccO <140800391+M1ccO@users.noreply.github.com>
2024-01-06 13:49:29 +0200

    Update Scroll-To-Top.js

--- Myth of Normal/JS/Scroll-To-Top.js
+++ Myth of Normal/JS/Scroll-To-Top.js
@@ -2,42 +2,27 @@
 
 
 function scrollToChapter() {
-    document.addEventListener('DOMContentLoaded', (event) => {
+    document.addEventListener('DOMContentLoaded', () => {
         var backToTopBtn = document.getElementById("button");
-        var firstH3 = document.querySelector('h3');
-        var buttonClicked = false; // Flag to track button click
 
-        function smoothScrollToChapter() {
-            if (firstH3) {
-                firstH3.scrollIntoView({ behavior: 'smooth', block: 'start' });
-                hideButton();
-            }
-        }
-
-        function hideButton() {
+        backToTopBtn.onclick = () => {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+            // Hide the button after clicking
             backToTopBtn.style.opacity = 0;
-            backToTopBtn.style.transform = 'translateY(100%)';
-            buttonClicked = true; // Set flag to true after hiding the button
-        }
+            backToTopBtn.style.transform = 'translateY(100%)'; // Slide out of view
+        };
 
-        backToTopBtn.onclick = smoothScrollToChapter;
-        backToTopBtn.addEventListener('touchend', smoothScrollToChapter);
-
-        setInterval(function() {
+        window.addEventListener('scroll', () => {
             if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                if (!buttonClicked) {
-                    backToTopBtn.style.opacity = 1;
-                    backToTopBtn.style.transform = 'translateY(0)'; // Slide into view
-                }
+                backToTopBtn.style.opacity = 1;
+                backToTopBtn.style.transform = 'translateY(0)'; // Slide into view
             } else {
-                buttonClicked = false; // Reset flag when at top of page
                 backToTopBtn.style.opacity = 0;
                 backToTopBtn.style.transform = 'translateY(100%)'; // Slide out of view
             }
-        }, 100);
+        });
     });
 }
 
 export { scrollToChapter };
-
 
