3621694c4c2ea5bb35f35e6f1f34bd2c69564d83
M1ccO <140800391+M1ccO@users.noreply.github.com>
2024-01-06 13:58:37 +0200

    Update Scroll-To-Top.js

--- Myth of Normal/JS/Scroll-To-Top.js
+++ Myth of Normal/JS/Scroll-To-Top.js
@@ -4,19 +4,27 @@
 function scrollToChapter() {
     document.addEventListener('DOMContentLoaded', () => {
         var backToTopBtn = document.getElementById("button");
+        var buttonClicked = false; // Flag to track if the button was clicked
 
         backToTopBtn.onclick = () => {
             window.scrollTo({ top: 0, behavior: 'smooth' });
-            // Hide the button after clicking
-            backToTopBtn.style.opacity = 0;
-            backToTopBtn.style.transform = 'translateY(100%)'; // Slide out of view
+            hideButton();
         };
+
+        function hideButton() {
+            backToTopBtn.style.opacity = 0;
+            backToTopBtn.style.transform = 'translateY(100%)'; // Hide button
+            buttonClicked = true; // Set flag to indicate button was clicked
+        }
 
         window.addEventListener('scroll', () => {
             if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                backToTopBtn.style.opacity = 1;
-                backToTopBtn.style.transform = 'translateY(0)'; // Slide into view
+                if (!buttonClicked) { // Only show button if it wasn't clicked
+                    backToTopBtn.style.opacity = 1;
+                    backToTopBtn.style.transform = 'translateY(0)'; // Slide into view
+                }
             } else {
+                buttonClicked = false; // Reset flag when at the top
                 backToTopBtn.style.opacity = 0;
                 backToTopBtn.style.transform = 'translateY(100%)'; // Slide out of view
             }
@@ -25,4 +33,3 @@ function scrollToChapter() {
 }
 
 export { scrollToChapter };
-
